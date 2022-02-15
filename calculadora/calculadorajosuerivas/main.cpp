106d944e6f418eb887f5f88a1d898cb547a806b1
Josué Rivas <jrivasd@miumg.edu.gt>
2022-02-14 22:26:01 -0600

    Calculadora2.0

--- calculadora/calculadorajosuerivas/main.cpp
+++ calculadora/calculadorajosuerivas/main.cpp
@@ -1,26 +1,95 @@
+//librerías
 #include <iostream>
+#include <wchar.h>
+#include <locale.h>
+#include <stdlib.h>
+
 
 using namespace std;
 
+
+//funciones
+double operacionSuma (double valorA, double valorB)
+{
+    return valorA+valorB;
+}
+
+double operacionResta (double valorA, double valorB)
+{
+    return valorA-valorB;
+}
+double operacionMulti (double valorA, double valorB)
+{
+    return valorA*valorB;
+}
+double operaciondiv (double valorA, double valorB)
+{
+    return valorA/valorB;
+}
+void mostrarResultado()
+{
+    cout << "El resultado de la operación es: ";
+}
+
+
+//main
 int main()
 {
-    double valora=0;
-    double valorb=0;
-    double valorsuma=0;
-    double valorresta=0;
-    double valormulti=0;
-    double valordiv=0;
-    cout << "Ingrese un primer valor: ";
-    cin >> valora;
-    cout << "Ingrese un segundo valor: ";
-    cin >> valorb;
-    valorsuma=valora+valorb;
-    valorresta=valora-valorb;
-    valormulti=valora*valorb;
-    valordiv=valora/valorb;
-    cout << "El resutado de la suma es: " << valorsuma << endl;
-    cout << "El resutado de la resta es: " << valorresta << endl;
-    cout << "El resutado de la multiplicación es: " << valormulti << endl;
-    cout << "El resutado de la división es: " << valordiv << endl;
+    //caracteres Especiales
+    setlocale(LC_ALL, "");
+
+
+    //variables
+    int menu=1;
+    int a=0;
+    double valorA=0;
+    double valorB=0;
+    double resultado=0;
+
+
+    //menu
+    while (menu!=0)
+    {
+        cout << "1. Suma" << endl << "2. Resta" << endl << "3. Multiplicación" << endl << "4. División" << endl << "0. Salir" << endl << "Ingrese el número que corresponde a la operación que desea realizar: ";
+        cin >> menu;
+        if (menu!=0){
+            cout << "Ingrese el primer valor: ";
+            cin >> valorA;
+            cout << "Ingrese el segundo valor: ";
+            cin >> valorB;
+        }
+        switch (menu)
+        {
+            case 1:
+                mostrarResultado();
+                cout << operacionSuma(valorA,valorB) << endl;
+            break;
+            case 2:
+                mostrarResultado();
+                cout << operacionResta(valorA,valorB)<< endl;
+            break;
+            case 3:
+                mostrarResultado();
+                cout << operacionMulti(valorA,valorB) << endl;
+            break;
+            case 4:
+                mostrarResultado();
+                cout << operaciondiv(valorA, valorB) << endl;
+            break;
+            case 0:
+                cout << "Nos vemos.";
+            break;
+            default:
+                cout << "El número ingresado no es válido, intente de nuevo.";
+            break;
+        }
+        if (menu!=0)
+        {
+            system("pause");
+            system("cls");
+        }
+    }
+
+
     return 0;
 }
