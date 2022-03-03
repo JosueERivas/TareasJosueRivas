25d18729bd38d472c9ee37755326de7eabcd8ac6
Josué Rivas <jrivasd@miumg.edu.gt>
2022-03-03 12:44:26 -0600

    Codigo limpio lenguaje c++

--- PrototipoP11P2022/9491-21-3133JosueRivas.cpp
+++ PrototipoP11P2022/9491-21-3133JosueRivas.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
-#include <stdio.h>
-#define N 3
 using namespace std;
 
 int main()
 {
-    int m1[N][N] = {{1, 1, 1}, {2, 2, 2}, {3, 3, 3}};
-    int m2[N][N];
-    int x, y;
-    for(x=0; x < N; x++)
-    for(y=0; y < N; y++)
-    if (m1[x][y]%2 == 0)
-    m2[x][y] = 0;
-    else
-    m2[x][y] = 1;
-    printf("%d , %d , %d", m2[0][0], m2[1][1], m2[2][2]);
+    int m1[3][3] = {{1, 1, 1}, {2, 2, 2}, {3, 3, 3}};
+    int m2[3][3];
+    int x=0;
+    int y=0;
+    int i=3;
+    for(x=0; x < i; x++)
+    {
+        for(y=0; y < i; y++)
+        {
+            if (m1[x][y]%2 == 0)
+            {
+                m2[x][y] = 0;
+            }
+            else
+            {
+                m2[x][y] = 1;
+            }
+        }
+    }
+    cout << "%d , %d , %d" << m2[0][0] << m2[1][1] << m2[2][2];
     return 0;
 }
