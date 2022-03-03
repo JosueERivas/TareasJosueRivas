2f9ec3a294064855bea339edba811e385f1202f6
Josué Rivas <jrivasd@miumg.edu.gt>
2022-03-03 13:02:01 -0600

    Mostrando valores ingresados y agregando comentarios

--- PrototipoP11P2022/9491-21-3133JosueRivas.cpp
+++ PrototipoP11P2022/9491-21-3133JosueRivas.cpp
@@ -1,27 +1,38 @@
+//librerias
 #include <iostream>
 using namespace std;
 
 int main()
 {
+    //declarando Matrices
     int m1[3][3] = {{1, 1, 1}, {2, 2, 2}, {3, 3, 3}};
     int m2[3][3];
+    //declarando Variables
     int x=0;
     int y=0;
+    //declarando Limite Del Ciclo
     int i=3;
+    //mensaje
+    cout << "Los valores que se ingresan en la matriz son los siguentes: " << endl;
+    //contador En "x"
     for(x=0; x < i; x++)
     {
+        //contador En "y"
         for(y=0; y < i; y++)
         {
+            //llenado De Matriz "m2"
             if (m1[x][y]%2 == 0)
             {
                 m2[x][y] = 0;
+                cout << m2[x][y];
             }
             else
             {
                 m2[x][y] = 1;
+                cout << m2[x][y];
             }
         }
+        cout << endl;
     }
-    cout << "%d , %d , %d" << m2[0][0] << m2[1][1] << m2[2][2];
     return 0;
 }
